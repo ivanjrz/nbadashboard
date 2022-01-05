4a9e65a6e2dd406c61196a960ac08d7806b9e088
Carlos Ivan <civjuarez@hotmail.com>
2022-01-04 20:57:15 -0800

    adding basic exceptions/

---
+++ NBAapi/Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
