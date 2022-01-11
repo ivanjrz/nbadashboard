47d5d4a0b6d5cd29527b7c4abfb45363e24706d7
Carlos Ivan <civjuarez@hotmail.com>
2022-01-10 22:35:58 -0800

    adding DTOs

---
+++ NBAapi/Contracts/PlayerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Contracts
+{
+    public class PlayerDto
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public DateTime Joined { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
---
+++ NBAapi/Contracts/UpdatePlayerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Contracts
+{
+    public class UpdatePlayerDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Team is required")]
+        [StringLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
+        public string Team { get; set; }
+
+        [Required(ErrorMessage = "Joining date is required")]
+        public DateTime Joined { get; set; }
+
+        public string PhotoPath { get; set; }
+    }
+}
