7b55793899d7dbffbe2e4546aad129d110123329
Carlos Ivan <civjuarez@hotmail.com>
2022-06-15 23:42:30 -0700

    updated routing, app.module and shared.serviceadded predictions API to services

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {PlayerComponent} from './player/player.component';
-import {TeamComponent} from './team/team.component';
-import {TeamHistoryComponent} from './teamhistory/teamhistory.component';
+import { PlayerComponent } from './player/player.component';
+import { TeamComponent } from './team/team.component';
+import { TeamHistoryComponent } from './teamhistory/teamhistory.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PredictionsComponent } from './predictions/predictions.component';
+import { ShowSeriesPredictionsComponent } from './predictions/show-series-predictions/show-series-predictions.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
 {path:'player',component:PlayerComponent},
 {path:'team',component:TeamComponent},
 {path:'teamhistory', component:TeamHistoryComponent},
+{path:'predictionsummary', component:PredictionsComponent},
+{path:'prediction', component:ShowSeriesPredictionsComponent},
+{path:'about', component:AboutComponent},
 {path:'', component:DashboardComponent}
 
 ];
